6cd3b5bf609709228325e9f2bfffd96f2192cfaf
Quentin Lassalle <qlassalle@gmail.com>
2016-09-27 14:35:26 +0200

    Add main class

---
+++ src/td00/Main.java
@@ -0,0 +1,11 @@
+package td00;
+
+/**
+ * Created by qlassalle on 27/09/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello TDD !");
+    }
+}
